3e0f0d19f775f25d5cbbcc28a0412bf288194610
WEBDEV-Rajat <162951475+WEBDEV-Rajat@users.noreply.github.com>
2024-11-11 22:42:13 +0530

    some changes in frontend

--- frontend/src/HomePage/Manage/Manage.jsx
+++ frontend/src/HomePage/Manage/Manage.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Navbar from '../Navigationbar';
+import "./Manage.css"
 
 const Manage = () => {
   return (
     <div>
       <Navbar/>
+      <div className='cat'>
+      <div className='inc-cat'>
+        <div className='manage-cats'>
+        <h1>Manage Income Categories</h1>
+        <button>+ Create New Category</button>
+        </div>
+        <div className="cats"></div>
+      </div>
+      <div className="exp-cat">
+      <div className='manage-cats'>
+        <h1>Manage Expense Categories</h1>
+        <button>+ Create New Category</button>
+        </div>
+        <div className="cats"></div>
+      </div>
+      </div>
     </div>
   )
 }
