b3169f0aee2853676099636a0af1c278f578991c
Varun <varunsahu10109@gmail.com>
2025-01-17 23:36:17 +0530

    error checked of google login

---
+++ frontend/src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyChsmjnfaL1b2YXPuktlhOHQ9SGwMWQBS0",
+  authDomain: "agile-avengers-234be.firebaseapp.com",
+  projectId: "agile-avengers-234be",
+  storageBucket: "agile-avengers-234be.firebasestorage.app",
+  messagingSenderId: "462633033551",
+  appId: "1:462633033551:web:a22e5c637279008ecc6f3a",
+  measurementId: "G-CEHE9ZNYXC",
+};
+export const app = initializeApp(firebaseConfig);
